0ed19a36142edaef3bffe180e8436b793df61f94
PierreVieira <pierrevieiraggg@gmail.com>
2020-01-04 13:37:11 -0300

    [+] Comentários

--- src/ex001.kt
+++ src/ex001.kt
@@ -1,3 +1,7 @@
+/**
+ * @author Pierre Vieira
+ * @since Jan - 2020
+ */
 fun main() {
-    print("Kotlin é\n\"SUPER\"\nFácil!")
+    print("Kotlin é\n\"SUPER\"\nFácil!") //Lembrando que para mostrar os caracteres especiais, basta incluir outra \
 }
